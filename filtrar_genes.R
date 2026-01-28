22f1e779ea9173e94f3018dcb302a5a48b97bc84
felipeasop <flpasop27@gmail.com>
2026-01-28 20:21:09 -0300

    Usando a tabela de perfis filtrados

--- filtrar_genes.R
+++ filtrar_genes.R
@@ -1,27 +1,20 @@
 if (!require("pacman")) install.packages("pacman")
-p_load(tidyverse, rsample)
+p_load(readr, dplyr, tidyr, rsample, conflicted)
 
-dados <- read_csv("norm_tibble.csv")
+conflicts_prefer(dplyr::filter)
 
-genes_filtrados <- dados %>% select(-id, -barcode, -condition)
+perfis <- read_csv("matriz_perfis_genes.csv", col_names = TRUE)
 
-genes_selecionados <- genes_filtrados %>% 
-  summarise(across(everything(), sd)) %>%
+calc_variacao_categorica <- function(coluna) {
+  return(length(unique(coluna)))
+}
+
+genes_variantes <- perfis %>%
+  summarise(across(-condition, calc_variacao_categorica)) %>%
   pivot_longer(everything(), names_to = "gene", values_to = "desvio_padrao") %>%
   arrange(desc(desvio_padrao)) %>%
-  slice_head(n = 500) %>% pull(gene)
+  slice_head(n = 500) %>%
+  pull(gene)
 
-
-dados_filtrados <- dados_brutos %>% 
-  select(condition, all_of(genes_selecionados))
-
-set.seed(1) 
-split_projeto <- initial_split(dados_filtrados, prop = 0.8, strata = condition)
-
-dados_treino <- training(split_projeto)
-dados_teste  <- testing(split_projeto)
-
-saveRDS(dados_treino, "dados_treino.rds")
-saveRDS(dados_teste,  "dados_teste.rds")
-
-print(table(dados_treino$condition)) 
+dados_filtrados <- perfis %>%
+  select(condition, all_of(genes_variantes))
