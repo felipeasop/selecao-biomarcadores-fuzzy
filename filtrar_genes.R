1c888b6c61c2cd902750a8b83a3b75d2008da901
felipeasop <flpasop27@gmail.com>
2026-01-28 21:29:19 -0300

    Variaveis corrigidas e exportação de dados treino e teste para csv

--- filtrar_genes.R
+++ filtrar_genes.R
@@ -1,5 +1,5 @@
 if (!require("pacman")) install.packages("pacman")
-p_load(readr, dplyr, tidyr, rsample, conflicted)
+p_load(readr, dplyr, tidyr, conflicted) 
 
 conflicts_prefer(dplyr::filter)
 
@@ -11,10 +11,18 @@ calc_variacao_categorica <- function(coluna) {
 
 genes_variantes <- perfis %>%
   summarise(across(-condition, calc_variacao_categorica)) %>%
-  pivot_longer(everything(), names_to = "gene", values_to = "desvio_padrao") %>%
-  arrange(desc(desvio_padrao)) %>%
+  pivot_longer(everything(), names_to = "gene", values_to = "variacao") %>%
+  arrange(desc(variacao)) %>%
   slice_head(n = 500) %>%
   pull(gene)
 
 dados_filtrados <- perfis %>%
   select(condition, all_of(genes_variantes))
+
+set.seed(1)
+indices <- sample(1:nrow(dados_filtrados), size = 0.8 * nrow(dados_filtrados))
+dados_treino <- dados_filtrados[indices, ]
+dados_teste  <- dados_filtrados[-indices, ]
+
+write_csv(dados_treino, "dados_treino.csv")
+write_csv(dados_teste,  "dados_teste.csv")
